4f1782f72f29a3e0df7c2df9c1d5bc9592230fb0
orangefeline <rodrigo.anothermen@gmail.com>
2024-12-03 17:43:33 -0600

    segundo commit

---
+++ css/script.js
@@ -0,0 +1,11 @@
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  console.log('mostrando');
+  sidebar.style.display = 'flex';
+}
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  console.log('ocultando');
+  sidebar.style.display = 'none';
+}
+
